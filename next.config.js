d052482227877cd5e2eb73d8ae4e3b8c2657cb1e
Eason <easoninus@gmail.com>
2023-07-13 11:30:15 +0800

    修改next config

--- next.config.js
+++ next.config.js
@@ -16,14 +16,14 @@ const nextConfig = {
   //     },
   //   ]
   // },
-  output: 'export',
+  // output: 'export',
   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  trailingSlash: true,
+  // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
+  // distDir: 'dist',
+  // images: {
+  //   unoptimized: true,
+  // },
 }
 
 module.exports = nextConfig
