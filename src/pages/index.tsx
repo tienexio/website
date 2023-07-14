62a4c1dec1419bad8d8d167af40b24d92cf1f009
yank <yank@hspaces.cn>
2023-07-14 17:03:53 +0800

    :移動端

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState,useEffect,useRef } from 'react';
 
 export default function Accordion() {
+  const [sharepop,setSharepop] = useState(false)
   const titleBar = ['Home','Docs']
   useEffect(() => {
     const scrollToTop = ()=>{
@@ -25,10 +26,10 @@ export default function Accordion() {
   return (
     <>
       <main className='home-bg-img w-full min-h-screen flex flex-col items-center'>
-        <div className='fixed top-0 bg-white w-full h-[70px] z-10 '>
-          <div className='flex items-center justify-between max-w-7xl dash:px-32 h-full text-[#453D6A] sm:px-2 m-auto'>
+        <div className='fixed top-0 bg-white w-full h-[70px] z-10'>
+          <div className='flex items-center justify-between max-w-7xl dash:px-32 h-full text-[#453D6A] sm:px-4 m-auto relative'>
             <div className='flex items-center gap-10 sm:gap-2'>
-              <img src="/img/logo.svg" className='w-28 h-9 sm:w-16 sm:h-6' />
+              <img src="/img/logo.svg" className='w-28 h-9 sm:w-20 sm:h-6' />
               {/* {titleBar.map((item,index)=>{
                 return <text key={item} className={`text-base ${index==0 && 'text-[blue]'} cursor-pointer`}>{item}</text>
               })} */}
@@ -36,54 +37,65 @@ export default function Accordion() {
 
             <div className='flex gap-3 items-center sm:gap-1'>
               {Array(1).fill('').map((item,index)=>{
-                return <img src={`/img/share${index}.svg`} alt="" className='w-6 h-6 cursor-pointer' onClick={()=>toshare(index)}/>
+                return <img src={`/img/share${index}.svg`} alt="" className='w-6 h-6 cursor-pointer sm:hidden' onClick={()=>toshare(index)}/>
               })}
-              <div className='flex justify-center items-center w-24 h-8 bg-[#0B5EFE] rounded-full text-[#ffffff] px-3 ml-3 sm:w-16 sm:h-6 sm:ml-1 cursor-pointer'>
+              <div className='flex justify-center items-center w-24 h-8 bg-[#0B5EFE] rounded-full text-[#ffffff] px-2 ml-3 sm:ml-1 cursor-pointer'>
                 <img src="/img/app_icon.svg" alt="" className='w-4'  />
                 <text className='sm:text-xs ml-2'>APP</text>
               </div>
+
+              <div className='smShare' onClick={()=>setSharepop(true)}></div>
             </div>
+
+            {
+              sharepop?<div className='sharePop' onClick={()=>setSharepop(false)}>
+                <div className='w-44 h-9 flex justify-between items-center' onClick={()=>toshare(0)}>
+                  <span>Twitter</span> 
+                  <img src={`/img/share0.svg`} alt="" className='w-6 h-6 cursor-pointer' />
+                </div>
+              </div>:null
+            }
           </div>
         </div>
 
-        <div className='w-full relative sm:mt-[70px] sm:h-80 bg-black'>
-          <img src="/img/head_bg.svg" className='w-full h-full  object-cover' />
-          <div className='w-full absolute left-0 top-1/2 -translate-y-1/2 text-[#3D4C6A]'>
-            <div className='max-w-7xl sm:w-80 sm:-translate-x-1/2 sm:px-2 sm:text-center mx-auto dash:px-32'>
-              <h1 className='text-7xl leading-snug font-semibold break-keep sm:text-base dash:text-6xl'>Tienex - Reshaping DeFi. Bridging traditional and decentralized finance.</h1>
-              <p className='text-2xl mt-4 leading-snug break-keep sm:text-sm dash:text-xl'>Tienex is a pioneer in the world of DeFi, providing a platform for creating, managing, and trading synthetic assets, including Real-World Assets (RWA). Embrace the democratized access to diverse financial markets made possible by blockchain technology.</p>
+        <div className='w-full relative sm:mt-[70px] sm:h-[560px] bg-black'>
+          <img src="/img/head_bg.svg" className='w-full h-full  object-cover ' />
+          <div className='w-full absolute left-0 top-1/2 -translate-y-1/2 text-[#3D4C6A] sm:top-52'>
+            <div className='max-w-7xl sm:px-6 sm:text-left mx-auto dash:px-32'>
+              <h1 className='text-7xl leading-snug font-semibold break-keep sm:text-2xl dash:text-6xl'>Tienex - Reshaping DeFi. Bridging traditional and decentralized finance.</h1>
+              <p className='text-2xl mt-4 break-keep sm:text-sm dash:text-xl'>Tienex is a pioneer in the world of DeFi, providing a platform for creating, managing, and trading synthetic assets, including Real-World Assets (RWA). Embrace the democratized access to diverse financial markets made possible by blockchain technology.</p>
             </div>
           </div>
         </div>
 
         <div className=' relative h-[700px] sm:h-[500px]'>
           <img src={`/img/asset_bg.svg`} className='w-full h-full' />
-          <div className={`absolute flex items-center justify-between top-0 w-full h-full sm:flex-col max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 sm:px-2`}>
-            <img src={`/img/asset0.svg`} className='w-[520px] h-[418px]' />
-            <div className='text-[#3D4C6A] w-[600px] sm:w-full sm:text-center'>
-              <p className='text-6xl break-keep relative leading-snug sm:text-base'>Synthetic Asset Management</p>
-              <p className='text-2xl break-keep mt-4 leading-snug sm:text-base'>Create, manage, and trade synthetic assets that mirror their real-world counterparts.</p>
+          <div className={`absolute flex items-center justify-between top-0 w-full h-full sm:flex-col-reverse max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 sm:px-4`}>
+            <img src={`/img/asset0.svg`} className='w-[520px] h-[418px] sm:w-[280px] sm:h-[225px]' />
+            <div className='text-[#3D4C6A] w-[600px] sm:w-full sm:text-left sm:mt-16'>
+              <p className='text-6xl break-keep relative leading-snug sm:text-2xl sm:w-2/3 font-semibold'>Synthetic Asset Management</p>
+              <p className='text-2xl break-keep mt-4 leading-snug sm:text-sm'>Create, manage, and trade synthetic assets that mirror their real-world counterparts.</p>
             </div>
           </div>
         </div>
 
-        <div className='w-full relative h-[700px] sm:h-[500px]'>
+        <div className='w-full relative h-[700px] sm:h-[600px]'>
           <div className='absolute top-0 max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 w-full h-full sm:px-2'>
-            <div className={` flex items-center justify-between w-full h-full sm:flex-col-reverse relative`}>
-              <div className='text-[#3D4C6A] w-[620px] sm:w-full sm:text-center'>
-                <p className='text-6xl break-keep relative leading-snug sm:text-base'>Tokenization of Real World Assets (RAW)</p>
+            <div className={` flex items-center justify-between w-full h-full sm:flex-col relative`}>
+              <div className='text-[#3D4C6A] w-[620px] sm:w-full sm:mt-16'>
+                <p className='text-6xl break-keep relative leading-snug sm:text-2xl font-semibold'>Tokenization of R eal World Assets (RAW)</p>
                 <p className='text-2xl break-keep mt-4 leading-snug sm:text-base'>Tokenize physical or abstract assets, allowing wider investment participation and improved asset liquidity.</p>
               </div>
-              <img src={`/img/asset1.svg`} className='w-[500px] h-[828px]  sm:top-0 sm:h-96' />
+              <img src={`/img/asset1.svg`} className='w-[500px] h-[828px] sm:w-80 sm:h-96' />
             </div>
           </div>
         </div>
 
         <div className='w-full relative h-[700px] sm:h-[500px]'>
-          <div className={`absolute flex items-center justify-between top-0 w-full h-full sm:flex-col max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 sm:px-2`}>
-          <img src={`/img/asset2.svg`} className='w-[530px] h-[616px]' />
-            <div className='text-[#3D4C6A] w-[600px] sm:w-full sm:text-center'>
-              <p className='text-6xl break-keep relative leading-snug sm:text-base'>Accessibility</p>
+          <div className={`absolute flex items-center justify-between top-0 w-full h-full sm:flex-col-reverse max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 sm:px-2 sm:justify-start`}>
+          <img src={`/img/asset2.svg`} className='w-[530px] h-[616px] sm:w-[300px] sm:h-[340px]' />
+            <div className='text-[#3D4C6A] w-[600px] sm:w-full sm:mt-16'>
+              <p className='text-6xl break-keep relative leading-snug sm:text-2xl font-semibold'>Accessibility</p>
               <p className='text-2xl break-keep mt-4 leading-snug sm:text-base'>Reduced geographical barriers and reliance on intermediaries thanks to blockchain technology.</p>
             </div>
           </div>
@@ -91,12 +103,12 @@ export default function Accordion() {
         
         <div className='w-full relative h-[700px] sm:h-[500px]'>
           <img src={`/img/asset_bg.svg`} className='w-full h-full' />
-          <div className={`absolute flex items-center justify-between top-0 w-full h-full sm:flex-col-reverse max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 sm:px-2 sm:justify-end`}>
-            <div className='text-[#3D4C6A] w-[600px] sm:w-full sm:text-center sm:-mt-10'>
-              <p className='text-6xl break-keep relative leading-snug sm:text-base '>Security</p>
+          <div className={`absolute flex items-center justify-between top-0 w-full h-full sm:flex-col max-w-7xl left-1/2 -translate-x-1/2 dash:px-32 sm:px-2 sm:justify-end`}>
+            <div className='text-[#3D4C6A] w-[600px] sm:w-full sm:mt-16'>
+              <p className='text-6xl break-keep relative leading-snug sm:text-2xl font-semibold '>Security</p>
               <p className='text-2xl break-keep mt-4 leading-snug sm:text-base'>Operates on the secure and efficient blockchain, ensuring trustless and safe transactions.</p>
             </div>
-            <img src={`/img/asset3.svg`} className='w-[702px] h-[864px] sm:h-[400px]' />
+            <img src={`/img/asset3.svg`} className='w-[702px] h-[864px] sm:h-[330px] sm:w-[350px]' />
           </div>
         </div>
 
@@ -119,12 +131,14 @@ export default function Accordion() {
             </div>
           </div> */}
           <div className='w-full h-[1px] bg-[#AEBCE0] '></div>
-          <div className='flex justify-between max-w-7xl m-auto dash:px-32 items-center h-20 text-[#616D8D] sm:px-2'>
+          <div className='flex justify-between max-w-7xl m-auto dash:px-32 items-center h-20 text-[#616D8D] sm:px-2 sm:flex-col sm:py-4 sm:h-auto'>
             <img src="/img/logo.svg" className='w-28 h-9' />
-            <p className='pr-24 sm:pr-0 sm:w-48'>Copyright 2023. All right reserved.</p>
+            <p className='pr-24 sm:pr-0 sm:mt-4'>Copyright 2023. All right reserved.</p>
           </div>
         </div>
       </main>
+
+      
     </>
   );
 }
